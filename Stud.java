2696a9e73cd3b2698477ef8af1937dfe89726577
tsavalove <tsavalove@gmail.com>
2020-11-18 15:38:48 +0200

    Added method hascovid

--- Stud.java
+++ Stud.java
@@ -1,13 +1,20 @@
 import java.util.*;
 import java.util.Scanner;
 public class Stud{
-    
+    static int cov;
     static int answ;
     static Scanner input = new Scanner(System.in);
     public static void queStud() {    
     System.out.println("If you have tested positive press 1 or else 0");
         answ = input.nextInt();
     }
+    public static boolean hascovid() {
+        if (cov==1) {
+            return true;
+        } else {
+             return false;
+        } //end else 
+       } // end hascovid
     public static void main(String[] args) {
         queStud();
     }
